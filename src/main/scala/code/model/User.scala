45f6ebdd57bb4c88ca62c3e0fbac345e9324b83e
Tomo Kazahaya <tomonacci@gmail.com>
2011-02-20 22:07:28 +0900

    Added administrator field to class User

--- src/main/scala/code/model/User.scala
+++ src/main/scala/code/model/User.scala
@@ -4,23 +4,17 @@ import _root_.net.liftweb.mapper._
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 
-/**
- * The singleton that has methods for accessing the database
- */
 object User extends User with MetaMegaProtoUser[User] {
   override def dbTableName = "users" // define the DB table name
   override def screenWrap = Full(<lift:surround with="default" at="content"><lift:bind /></lift:surround>)
   // define the order fields will appear in forms and output
-  override def fieldOrder = List(id, firstName, lastName, email,
-  locale, timezone, password)
+  override def fieldOrder = List(id, administrator, firstName, lastName, email, locale, timezone, password)
 
   // comment this line out to require email validations
   override def skipEmailValidation = true
 }
 
-/**
- * An O-R mapped "User" class that includes first name, last name, password
- */
 class User extends MegaProtoUser[User] {
-  def getSingleton = User // what's the "meta" server
+  def getSingleton = User
+  object administrator extends MappedBoolean(this)
 }
